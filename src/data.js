fd30abcfb68121b8140b11c7d280c1d57b293c5a
Hypermona <mohans8050@gmail.com>
2021-05-23 22:09:13 +0530

    Feat: add sample data

---
+++ src/data.js
@@ -0,0 +1,138 @@
+import blackFungus from "./Images/black-fungus.jpg";
+import mohan from "./Images/mohan.jfif";
+
+export const POSTS = [
+  {
+    image: blackFungus,
+    title:
+      "Black Fungus Treatment: 3 simple oral hygiene tips you must follow to prevent this fungal infection",
+    shortDescription:
+      "Also called Mucormycosis, Black Fungus is a disease that is commonly found in people who have recently recovered from Covid-19.",
+    description:
+      "On rapidly spreading infection post–COVID recovery is black fungus. Also called Mucormycosis, Black Fungus is a disease that is commonly found in people who have recently recovered from Covid-19, been on oxygen support for a long time, or have deranged sugar levels. It may also be caused due to poor hygiene of critical equipment like an oxygen mask.\nHowever, following some simple oral hygiene tips can reduce the chances of catching this rare yet fatal disease according to a dentist as primary black fungus symptoms include discolouration of the oral tissues, tongue, and gums. Other symptoms of the infection are a stuffy nose, severe pain, swelling of the face, heaviness below the eyes, discomfort, fever, and headache.",
+    author: "_itshyper__",
+    likes: 0,
+    dislikes: 0,
+    shares: 0,
+    saves: 0,
+    comments: [],
+    dateAndTime: "Sun May 23 2021 21:48:49 GMT+0530 (India Standard Time)",
+  },
+  {
+    image: blackFungus,
+    title:
+      "Black Fungus Treatment: 3 simple oral hygiene tips you must follow to prevent this fungal infection",
+    shortDescription:
+      "Also called Mucormycosis, Black Fungus is a disease that is commonly found in people who have recently recovered from Covid-19.",
+    description:
+      "On rapidly spreading infection post–COVID recovery is black fungus. Also called Mucormycosis, Black Fungus is a disease that is commonly found in people who have recently recovered from Covid-19, been on oxygen support for a long time, or have deranged sugar levels. It may also be caused due to poor hygiene of critical equipment like an oxygen mask.\nHowever, following some simple oral hygiene tips can reduce the chances of catching this rare yet fatal disease according to a dentist as primary black fungus symptoms include discolouration of the oral tissues, tongue, and gums. Other symptoms of the infection are a stuffy nose, severe pain, swelling of the face, heaviness below the eyes, discomfort, fever, and headache.",
+    author: "_itshyper__",
+    likes: 0,
+    dislikes: 0,
+    shares: 0,
+    saves: 0,
+    comments: [],
+    dateAndTime: "Sun May 23 2021 21:48:49 GMT+0530 (India Standard Time)",
+  },
+  {
+    image: blackFungus,
+    title:
+      "Black Fungus Treatment: 3 simple oral hygiene tips you must follow to prevent this fungal infection",
+    shortDescription:
+      "Also called Mucormycosis, Black Fungus is a disease that is commonly found in people who have recently recovered from Covid-19.",
+    description:
+      "On rapidly spreading infection post–COVID recovery is black fungus. Also called Mucormycosis, Black Fungus is a disease that is commonly found in people who have recently recovered from Covid-19, been on oxygen support for a long time, or have deranged sugar levels. It may also be caused due to poor hygiene of critical equipment like an oxygen mask.\nHowever, following some simple oral hygiene tips can reduce the chances of catching this rare yet fatal disease according to a dentist as primary black fungus symptoms include discolouration of the oral tissues, tongue, and gums. Other symptoms of the infection are a stuffy nose, severe pain, swelling of the face, heaviness below the eyes, discomfort, fever, and headache.",
+    author: "_itshyper__",
+    likes: 0,
+    dislikes: 0,
+    shares: 0,
+    saves: 0,
+    comments: [],
+    dateAndTime: "Sun May 23 2021 21:48:49 GMT+0530 (India Standard Time)",
+  },
+  {
+    image: blackFungus,
+    title:
+      "Black Fungus Treatment: 3 simple oral hygiene tips you must follow to prevent this fungal infection",
+    shortDescription:
+      "Also called Mucormycosis, Black Fungus is a disease that is commonly found in people who have recently recovered from Covid-19.",
+    description:
+      "On rapidly spreading infection post–COVID recovery is black fungus. Also called Mucormycosis, Black Fungus is a disease that is commonly found in people who have recently recovered from Covid-19, been on oxygen support for a long time, or have deranged sugar levels. It may also be caused due to poor hygiene of critical equipment like an oxygen mask.\nHowever, following some simple oral hygiene tips can reduce the chances of catching this rare yet fatal disease according to a dentist as primary black fungus symptoms include discolouration of the oral tissues, tongue, and gums. Other symptoms of the infection are a stuffy nose, severe pain, swelling of the face, heaviness below the eyes, discomfort, fever, and headache.",
+    author: "_itshyper__",
+    likes: 0,
+    dislikes: 0,
+    shares: 0,
+    saves: 0,
+    comments: [],
+    dateAndTime: "Sun May 23 2021 21:48:49 GMT+0530 (India Standard Time)",
+  },
+  {
+    image: blackFungus,
+    title:
+      "Black Fungus Treatment: 3 simple oral hygiene tips you must follow to prevent this fungal infection",
+    shortDescription:
+      "Also called Mucormycosis, Black Fungus is a disease that is commonly found in people who have recently recovered from Covid-19.",
+    description:
+      "On rapidly spreading infection post–COVID recovery is black fungus. Also called Mucormycosis, Black Fungus is a disease that is commonly found in people who have recently recovered from Covid-19, been on oxygen support for a long time, or have deranged sugar levels. It may also be caused due to poor hygiene of critical equipment like an oxygen mask.\nHowever, following some simple oral hygiene tips can reduce the chances of catching this rare yet fatal disease according to a dentist as primary black fungus symptoms include discolouration of the oral tissues, tongue, and gums. Other symptoms of the infection are a stuffy nose, severe pain, swelling of the face, heaviness below the eyes, discomfort, fever, and headache.",
+    author: "_itshyper__",
+    likes: 0,
+    dislikes: 0,
+    shares: 0,
+    saves: 0,
+    comments: [],
+    dateAndTime: "Sun May 23 2021 21:48:49 GMT+0530 (India Standard Time)",
+  },
+];
+
+export const USERS = [
+  {
+    profilePic: mohan,
+    name: "Mohana S",
+    userName: "_itshyper__",
+    email: "mohans8050@gmail.com",
+    followers: 20,
+    following: 3,
+    verified: true,
+    joined: "Sun May 23 2021 21:48:49 GMT+0530 (India Standard Time)",
+  },
+  {
+    profilePic: mohan,
+    name: "Mohana S",
+    userName: "_itshyper__",
+    email: "mohans8050@gmail.com",
+    followers: 20,
+    following: 3,
+    verified: true,
+    joined: "Sun May 23 2021 21:48:49 GMT+0530 (India Standard Time)",
+  },
+  {
+    profilePic: mohan,
+    name: "Mohana S",
+    userName: "_itshyper__",
+    email: "mohans8050@gmail.com",
+    followers: 20,
+    following: 3,
+    verified: true,
+    joined: "Sun May 23 2021 21:48:49 GMT+0530 (India Standard Time)",
+  },
+  {
+    profilePic: mohan,
+    name: "Mohana S",
+    userName: "_itshyper__",
+    email: "mohans8050@gmail.com",
+    followers: 20,
+    following: 3,
+    verified: true,
+    joined: "Sun May 23 2021 21:48:49 GMT+0530 (India Standard Time)",
+  },
+  {
+    profilePic: mohan,
+    name: "Mohana S",
+    userName: "_itshyper__",
+    email: "mohans8050@gmail.com",
+    followers: 20,
+    following: 3,
+    verified: true,
+    joined: "Sun May 23 2021 21:48:49 GMT+0530 (India Standard Time)",
+  },
+];
