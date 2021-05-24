7c542f7220232b4cce487dd59970f281373deefc
Hypermona <mohans8050@gmail.com>
2021-05-24 13:03:30 +0530

    Feat: change colors

--- src/Custom/Customization.jsx
+++ src/Custom/Customization.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 const theme = createMuiTheme({
   palette: {
     background: {
       default: "#263238",
     },
-    type: "dark",
+    type: "light",
     primary: {
-      main: "#4527a0",
+      main: "#640DA8",
     },
     secondary: {
-      main: "#fdd835",
+      main: "#E2F05D",
     },
   },
 });
 
 export default function Customization({ children }) {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = createMuiTheme({
+    palette: {
+      background: {
+        default: prefersDarkMode ? "#442A57" : "#C77DFF",
+      },
+      type: prefersDarkMode ? "dark" : "light",
+      primary: {
+        main: "#3C0863",
+      },
+      secondary: {
+        main: "#CADB2A",
+      },
+    },
+  });
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import posts from "./posts";
+import users from "./users";
+import theme from "./theme";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    posts,
+    users,
+    theme,
+  },
 });
