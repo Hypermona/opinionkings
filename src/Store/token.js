6f166aab1f82dda7945602b2e17150f1dea0b0e0
Hypermona <mohans8050@gmail.com>
2021-06-19 14:08:08 +0530

    Feat: add user id

--- src/Store/token.js
+++ src/Store/token.js
@@ -1,5 +1,14 @@
 const AUTH_TOKEN = "auth-token";
+const USER_ID = "auth-user";
 
 export const getToken = () => localStorage.getItem(AUTH_TOKEN);
-export const setToken = (token) => localStorage.setItem(AUTH_TOKEN, token);
-export const deleteToken = () => localStorage.removeItem(AUTH_TOKEN);
+export const getUser = () => localStorage.getItem(USER_ID);
+
+export const setToken = (token, id) => {
+  localStorage.setItem(AUTH_TOKEN, token);
+  localStorage.setItem(USER_ID, id);
+};
+export const deleteToken = () => {
+  localStorage.removeItem(AUTH_TOKEN);
+  localStorage.removeItem(USER_ID);
+};
