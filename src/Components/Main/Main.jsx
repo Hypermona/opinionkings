2c4e2fbe71685ee6fa186665faa5a4089c235dfc
Hypermona <mohans8050@gmail.com>
2021-06-24 11:57:11 +0530

    Fix: change normal ip to elastic ip

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -6,7 +6,7 @@ import { createClient, Provider } from "urql";
 import { getToken } from "../../Store/token";
 
 const client = createClient({
-  url: "http://ec2-13-233-96-11.ap-south-1.compute.amazonaws.com:4000/",
+  url: "https://ec2-3-108-82-42.ap-south-1.compute.amazonaws.com/",
   fetchOptions: () => {
     const token = getToken();
     return {
