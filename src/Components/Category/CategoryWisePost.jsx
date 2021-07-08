ddab6966f01e2da2e5312ed69d63a0592c4d2755
Hypermona <mohans8050@gmail.com>
2021-07-08 23:22:48 +0530

    Refactor: category css file

---
+++ src/Components/Category/CategoryWisePost.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Post from "../Posts/Post";
+
+function CategoryWisePost() {
+  return (
+    <div>
+      <Post />
+    </div>
+  );
+}
+
+export default CategoryWisePost;
